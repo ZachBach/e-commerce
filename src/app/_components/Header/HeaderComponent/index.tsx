cfb3960293128336e1bd5d6a741587796d15f6a3
Zach Auerbach <zachauerbachdev@gmail.com>
2024-04-12 12:40:31 -0700

    added navbar/working on mobile nav

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+
 
 const HeaderComponent = ({ header }: { header: Header }) => {
     return (
-        <nav>
-            <Gutter>
+        <nav className={classes.header}>
+            <Gutter className={classes.wrap}>
                 <Link href="/">
-                <img src="/logo-black.svg" alt="logo" />
+                <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
                 </Link>
+
+                <HeaderNav header={header}/>
+                <HeaderMobileNav header={header} />
             </Gutter>
         </nav>
     )
